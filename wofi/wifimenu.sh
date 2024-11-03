e39ee418faae0784006255cfefb66e7680fdbbff
Matheus Wesley <matheus.tarra@gmail.com>
2024-11-03 13:41:59 -0300

    Ajustes em menus, waybar e programa padrão

--- wofi/wifimenu.sh
+++ wofi/wifimenu.sh
@@ -1,98 +1,107 @@
 #!/usr/bin/env bash
 
-# Starts a scan of available broadcasting SSIDs
-# nmcli dev wifi rescan
-
+# Variáveis de configuração
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-FIELDS=SSID,SECURITY
+FIELDS=SSID,SECURITY    # Adicionado BARS e RATE para mostrar força do sinal e velocidade
 POSITION=3
 YOFF=20
 XOFF=-40
+WOFI_WIDTH=400
+MAX_LINES=8
 
+# Carrega configurações personalizadas
 if [ -r "$DIR/config" ]; then
-	source "$DIR/config"
+    source "$DIR/config"
 elif [ -r "$HOME/.config/wofi/wifi" ]; then
-	source "$HOME/.config/wofi/wifi"
+    source "$HOME/.config/wofi/wifi"
 else
-	echo "WARNING: config file not found! Using default values."
+    echo "AVISO: arquivo de configuração não encontrado! Usando valores padrão."
 fi
 
-LIST=$(nmcli --fields "$FIELDS" device wifi list | sed '/^--/d')
-# For some reason wofi always approximates character width 2 short... hmmm
-RWIDTH=$(($(echo "$LIST" | head -n 1 | awk '{print length($0); }')*20))
-# Dynamically change the height of the wofi menu
-LINENUM=$(echo "$LIST" | wc -l)
-# Gives a list of known connections so we can parse it later
-KNOWNCON=$(nmcli connection show)
-# Really janky way of telling if there is currently a connection
-CONSTATE=$(nmcli -fields WIFI g)
+# Força uma nova varredura de redes WiFi disponíveis
+nmcli device wifi rescan || true
 
+# Obtém a lista de redes WiFi
+LIST=$(nmcli --fields "$FIELDS" device wifi list | sed '/^--/d')
+RWIDTH=$(($(echo "$LIST" | head -n 1 | awk '{print length($0); }')*20))
+LINENUM=$(echo "$LIST" | wc -l)
+
+# Obtém conexões conhecidas e estado atual
+KNOWNCON=$(nmcli connection show)
+WIFI_STATE=$(nmcli -fields WIFI g)
 CURRSSID=$(LANGUAGE=C nmcli -t -f active,ssid dev wifi | awk -F: '$1 ~ /^yes/ {print $2}')
 
-if [[ ! -z $CURRSSID ]]; then
-	HIGHLINE=$(echo  "$(echo "$LIST" | awk -F "[  ]{2,}" '{print $1}' | grep -Fxn -m 1 "$CURRSSID" | awk -F ":" '{print $1}') + 1" | bc )
+# Destaca a rede atual
+if [[ -n $CURRSSID ]]; then
+    HIGHLINE=$(echo "$LIST" | awk -F "[  ]{2,}" '{print $1}' | awk '{print NR}' "$CURRSSID")
 fi
 
-# HOPEFULLY you won't need this as often as I do
-# If there are more than 8 SSIDs, the menu will still only have 8 lines
-if [ "$LINENUM" -gt 8 ] && [[ "$CONSTATE" =~ "enabled" ]]; then
-	LINENUM=8
-elif [[ "$CONSTATE" =~ "disabled" ]]; then
-	LINENUM=1
+# Ajusta número de linhas do menu
+if [ "$LINENUM" -gt "$MAX_LINES" ] && [[ "$WIFI_STATE" =~ "enabled" ]]; then
+    LINENUM=$MAX_LINES
+elif [[ "$WIFI_STATE" =~ "disabled" ]]; then
+    LINENUM=1
 fi
 
-
-if [[ "$CONSTATE" =~ "enabled" ]]; then
-	TOGGLE="toggle off"
-elif [[ "$CONSTATE" =~ "disabled" ]]; then
-	TOGGLE="toggle on"
+# Define texto do botão toggle
+TOGGLE="Ativar WiFi"
+if [[ "$WIFI_STATE" =~ "enabled" ]]; then
+    TOGGLE="Desativar WiFi"
 fi
 
+# Mostra menu wofi com opções
+CHENTRY=$(echo -e "$TOGGLE\nConexão Manual\n$LIST" | uniq -u | \
+    wofi -i -d \
+    --prompt "Selecione uma rede WiFi: " \
+    --lines "$MAX_LINES" \
+    --width "$WOFI_WIDTH")
 
-CHENTRY=$(echo -e "$TOGGLE\nmanual\n$LIST" | uniq -u | wofi -i -d --prompt "Wi-Fi SSID: " --lines "8" --width 400)
-#echo "$CHENTRY"
-CHSSID=$(echo "$CHENTRY" | sed  's/\s\{2,\}/\|/g' | awk -F "|" '{print $1}')
-#echo "$CHSSID"
+# Extrai SSID selecionado
+CHSSID=$(echo "$CHENTRY" | sed 's/\s\{2,\}/\|/g' | awk -F "|" '{print $1}')
 
-# If the user inputs "manual" as their SSID in the start window, it will bring them to this screen
-if [ "$CHENTRY" = "manual" ] ; then
-	# Manual entry of the SSID and password (if appplicable)
-	MSSID=$(echo "enter the SSID of the network (SSID,password)" | wofi -d "Manual Entry: " --lines 1)
-	# Separating the password from the entered string
-	MPASS=$(echo "$MSSID" | awk -F "," '{print $2}')
-
-	#echo "$MSSID"
-	#echo "$MPASS"
-
-	# If the user entered a manual password, then use the password nmcli command
-	if [ "$MPASS" = "" ]; then
-		nmcli dev wifi con "$MSSID"
-	else
-		nmcli dev wifi con "$MSSID" password "$MPASS"
-	fi
-
-elif [ "$CHENTRY" = "toggle on" ]; then
-	nmcli radio wifi on
-
-elif [ "$CHENTRY" = "toggle off" ]; then
-	nmcli radio wifi off
-
-else
-
-	# If the connection is already in use, then this will still be able to get the SSID
-	if [ "$CHSSID" = "*" ]; then
-		CHSSID=$(echo "$CHENTRY" | sed  's/\s\{2,\}/\|/g' | awk -F "|" '{print $3}')
-	fi
-
-	# Parses the list of preconfigured connections to see if it already contains the chosen SSID. This speeds up the connection process
-	if [[ $(echo "$KNOWNCON" | grep "$CHSSID") = "$CHSSID" ]]; then
-		nmcli con up "$CHSSID"
-	else
-		if [[ "$CHENTRY" =~ "WPA2" ]] || [[ "$CHENTRY" =~ "WEP" ]]; then
-			WIFIPASS=$(echo "if connection is stored, hit enter" | wofi -P -d --prompt "password" --lines 1 --location "$POSITION" --yoffset "$YOFF" --xoffset "$XOFF" --width $RWIDTH)
-		fi
-		nmcli dev wifi con "$CHSSID" password "$WIFIPASS"
-	fi
-
-fi
+# Processa a escolha do usuário
+case "$CHENTRY" in
+    "Conexão Manual")
+        # Entrada manual de SSID e senha
+        MSSID=$(echo "Digite o SSID da rede (SSID,senha)" | \
+            wofi -d "Conexão Manual: " --lines 1)
+        MPASS=$(echo "$MSSID" | awk -F "," '{print $2}')
+        
+        if [ -z "$MPASS" ]; then
+            nmcli dev wifi con "$MSSID"
+        else
+            nmcli dev wifi con "$MSSID" password "$MPASS"
+        fi
+        ;;
+        
+    "Ativar WiFi")
+        nmcli radio wifi on
+        ;;
+        
+    "Desativar WiFi")
+        nmcli radio wifi off
+        ;;
+        
+    *)
+        # Conecta à rede selecionada
+        if [ "$CHSSID" = "*" ]; then
+            CHSSID=$(echo "$CHENTRY" | sed 's/\s\{2,\}/\|/g' | awk -F "|" '{print $3}')
+        fi
+        
+        if echo "$KNOWNCON" | grep -q "$CHSSID"; then
+            nmcli con up "$CHSSID"
+        else
+            if [[ "$CHENTRY" =~ "WPA2" ]] || [[ "$CHENTRY" =~ "WEP" ]]; then
+                WIFIPASS=$(echo "Pressione Enter se a conexão já estiver salva" | \
+                    wofi -P -d \
+                    --prompt "Senha:" \
+                    --lines 1 \
+                    --location "$POSITION" \
+                    --yoffset "$YOFF" \
+                    --xoffset "$XOFF" \
+                    --width "$RWIDTH")
+            fi
+            nmcli dev wifi con "$CHSSID" password "$WIFIPASS"
+        fi
+        ;;
+esac
